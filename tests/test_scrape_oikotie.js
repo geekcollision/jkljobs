e3629bc51858d8365e6d9cbe24d00f40c8311e1b
Juho Vepsalainen <bebraw@gmail.com>
2014-05-16 12:57:09 +0300

    Scrape oikotie

---
+++ tests/test_scrape_oikotie.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var fs = require('fs');
+
+var scrape = require('../lib/scrape_oikotie');
+
+
+tests();
+
+function tests() {
+    fs.readFile('data/oikotie.html', {
+        encoding: 'utf8'
+    }, function(err, d) {
+        if(err) {
+            return console.error(err);
+        }
+
+        scrape(d, function(err, d) {
+            if(err) {
+                return console.error(err);
+            }
+
+            console.log(d);
+        });
+    });
+}
