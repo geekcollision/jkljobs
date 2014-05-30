0861e21eb03bfb7b0b7fecc186ebcda49a92f7d1
Juho Vepsalainen <bebraw@gmail.com>
2014-05-30 10:43:36 +0300

    eilakaisla - Improve description

--- scrapers/eilakaisla/scraper.js
+++ scrapers/eilakaisla/scraper.js
@@ -11,12 +11,15 @@ function scrape(data) {
     var $ = cheerio.load(data);
     var $data = $('#content');
 
+    // get rid of company introductions and job application section
+    $('em, .apply-for-job').remove();
+
     return {
         title: $data.find('h1').text().split('-').slice(0, -1).join('-').trim(),
         gid: 'eilakaisla' + $data.find('h2').first().text().split('Työpaikan numero:').join('').trim(),
         type: getP($data, 1),
         added: moment(getP($data, 2), 'DD-MM-YYYY').utc().format(),
-        description: $data.find('p').eq(4).text()
+        description: $data.find('p').eq(3).nextAll().text().trim()
     };
 }
 
--- tests/test_eilakaisla_scraper.js
+++ tests/test_eilakaisla_scraper.js
@@ -10,6 +10,11 @@ var scrape = require('../scrapers/eilakaisla/scraper').scrape;
 tests();
 
 function tests() {
+    test1();
+    test2();
+}
+
+function test1() {
     fs.readFile('./data/eilakaisla.html', {
         encoding: 'utf8'
     }, function(err, d) {
@@ -25,5 +30,26 @@ function tests() {
         assert.equal(res.title, 'Ohjelmistosuunnittelija/ -testaaja');
         assert.equal(res.type, 'Toistaiseksi voimassa oleva');
         assert.equal(res.added, '2014-05-19T21:00:00+00:00');
+        assert(res.description.length > 0);
+    });
+}
+
+function test2() {
+    fs.readFile('./data/eilakaisla2.html', {
+        encoding: 'utf8'
+    }, function(err, d) {
+        if(err) {
+            return console.error(err);
+        }
+
+        var res = scrape(d);
+
+        console.log(res);
+
+        assert.equal(res.gid, 'eilakaisla6179');
+        assert.equal(res.title, 'Automaatio-ohjelmistosuunnittelija');
+        assert.equal(res.type, 'Toistaiseksi voimassa oleva');
+        assert.equal(res.added, '2014-05-22T21:00:00+00:00');
+        assert(res.description.length > 0);
     });
 }
